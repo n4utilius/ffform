fc307b05615832aa9c4b99084da67cca1149780e
Kevin Martinez <n4utilius@gmail.com>
2015-06-11 06:21:31 -0500

    Resuelto lo de bd a builder interface

--- assets/js/basic-example.js
+++ assets/js/basic-example.js
@@ -3,6 +3,7 @@
 
   angular.module('demoApp')
     .controller('BasicExampleCtrl', ['$scope', '$http', function ($scope, $http) {
+
       $scope.remove = function(scope) {
         scope.remove();
       };
@@ -33,133 +34,63 @@
         $scope.$broadcast('expandAll');
       };
 
-      $http.get('http://localhost:1337/question').success(function(data) {
+      var merge = function(A, B){
         /* Devuelve el objeto resultante de mezclar dos objetos*/
-        var merge = function(A, B){
-          for(var k in B) if(!(k in A)) A[k] = B[k]
-          return A;
-        }
-
-        var add_to_branch = function(node, nodes, tree){
-          var _tree = tree;
-          console.log(node, nodes, tree)
-          for(var i in tree){
-
-            // Si encuentra que hay un rama en el arbol con su mismo id (por que un hijo lo 'aparto'), entonces se mezcla con ella
-            if(node.id == tree[i].id){
-              tree[i] = merge(tree[i], node)
-              break
-
-            // Si tiene dependencia
-            }else if(node.dependency && node.dependency.id){
-
-              // si encuentra a su padre
-              if(node.dependency.id == tree[i].id){
-                // añadelo al padre
-                  if(tree[i].nodes && tree[i].nodes.length > 0){
-                    tree[i].nodes.push(node)
-                    break;
-                  }else{
-                    tree[i].nodes =[node]
-                    break;
-                  }
-                
-              }
-
-              // De lo contrario busca entre los hijos de la rama actual
-              else if(tree[i].nodes && tree[i].nodes.length > 0){
-                add_to_branch(node, tree[i].nodes)
-              } 
-            }else{
-              tree.push(node);
-              break
-            }
-          } 
-
-          if(tree !== _tree) 
-            return { 'tree': tree, "node": null }
-          else 
-            return { 'tree': null, "node": node }
-        }
-
-        var add_to_tree = function(nodes, tree){
-          // si no hay más nodos, devuelve el arbol
-          if(nodes.length < 1) return tree
-          var node = nodes.shift()
-
-          var d = add_to_branch(node, nodes, tree)
-          console.log(d)
-          if(!d.tree) nodes.push(d.node)
-
-          add_to_tree(nodes, tree);
-
-        }
-
-        var tree = add_to_tree(data, [])
+        for(var k in B) if(!(k in A)) A[k] = B[k];
+        return A;
+      }
         
-        console.log(data)
-      });
+      var search_and_add = function(node, tree){
+        for(var i in tree){
+          // Si encuentra que hay un rama en el arbol con su mismo id (por que un hijo lo 'aparto'), entonces se mezcla con ella
+          if(node.id == tree[i].id){
+            tree[i] = merge(tree[i], node);
+            break;
 
-
-
-      $scope.data = [
-        {
-          "id": "55767221ac4ec3825d33a891",
-          "title": "Nombre",
-          "type": "string",
-          "enable": true,
-          "nodes": [],
-        },
-        {
-          "id": "5576727dac4ec3825d33a892",
-          "title": "Edad",
-          "type": "integer",
-          "enable": true,
-          "nodes": [],
-        },
-        {
-          "id": "557672c8ac4ec3825d33a893",
-          "title": "Sexo",
-          "type": "string",
-          "enable": true,
-          "choices": ["Masculino", "Femenino", "Otro"],
-          "nodes": [],
-        },
-        {
-          "id": "55767344ac4ec3825d33a894",
-          "title": "¿Dio datos de Contacto?",
-          "type": "string",
-          "enable": true,
-          "choices": ["Si", "No"],
-          "nodes": [
-            {
-              "id": "5576741bac4ec3825d33a895",
-              "title": "Twitter",
-              "type": "string",
-              "enable": true,
-              "if_parent":"Si",
-              "nodes": [],
-            },
-            {
-              "id": "5576743bac4ec3825d33a896",
-              "title": "Correo",
-              "type": "string",
-              "enable": true,
-              "if_parent":"Si",
-              "nodes": [],
-            },
-            {
-              "id": "5576745bac4ec3825d33a897",
-              "title": "Teléfono",
-              "type": "string",
-              "if_parent":"Si",
-              "enable": true,
-              "nodes": [],
+          // Si tiene dependencia (o un padre)
+          }else if(node.dependency && node.dependency.id){
+            // si encuentra a su padre
+            if(node.dependency.id == tree[i].id){
+              // añadelo al padre
+              if(tree[i].nodes && tree[i].nodes.length > 0){
+                tree[i].nodes.push(node);
+                break;
+              }else{
+                tree[i].nodes =[node];
+                break;
+              }
             }
-          ],
-        }
-      ];
 
+            // De lo contrario busca entre los hijos de la rama actual
+            else if(tree[i].nodes && tree[i].nodes.length > 0){
+              tree = aux(node, tree[i].nodes)
+            } 
+
+          }
+        }
+
+        return tree
+      }
+
+      var add_to_branch = function(nodes, tree){
+        var node = nodes.shift();
+        if( tree.length < 1) tree.push(node);
+        else tree = search_and_add(node, tree);
+        add_to_tree( nodes, tree );
+      }
+
+      var add_to_tree = function(nodes, tree){
+        // si no hay más nodos, devuelve el arbol
+        if(nodes.length < 1) return tree;
+        add_to_branch(nodes, tree);
+      }
+
+      $http.get('http://localhost:1337/question').success(function(data) {
+        var tree = add_to_tree(data, []);
+        console.log(tree);
+
+        
+      });
 
     }]);
 
